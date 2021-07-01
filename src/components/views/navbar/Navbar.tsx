290b5b1b12a9de194a11a32a2841dbbcb6614fbb
jiye-7 <jy522hi@gmail.com>
2021-07-02 02:27:01 +0900

    :lipstick: Update Project Modal
    project 목록 보기 클릭 시 프로젝트 목록과 새 프로젝트를 만들 수 있는 모달창 추가

--- src/components/views/navbar/Navbar.tsx
+++ src/components/views/navbar/Navbar.tsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import styles from './sections/navbar.module.css';
 import profileImg from './sections/profile_img.png';
 import mainLogo2 from './sections/main_logo2.png';
 
 const Navbar = () => {
 	const [project, setProject] = useState();
-	const [isClicked, setIsClick] = useState();
+	const [isProjectClicked, setProjectClick] = useState();
+	const [isSearchClicked, setSearchClick] = useState();
 
-	const isClickedProject = (e: any) => {
-		console.log(e.target);
-	};
+	const isProjectClick = useCallback(
+		(e: any) => {
+			console.log(e.target);
+		},
+		[isProjectClicked],
+	);
+
+	const isSearchClick = useCallback(
+		(e: any) => {
+			console.log(e.target);
+		},
+		[isSearchClicked],
+	);
 
 	return (
 		<nav className={styles.container}>
@@ -19,7 +30,7 @@ const Navbar = () => {
 						<img src={mainLogo2} alt="main_logo" />
 					</div>
 					<div className={styles.project}>
-						<button className={styles.project_button} onClick={isClickedProject}>
+						<button className={styles.project_button} onClick={isProjectClick}>
 							프로젝트 목록 보기
 							{/* <i className="fas fa-caret-down"></i> */}
 						</button>
@@ -27,7 +38,9 @@ const Navbar = () => {
 				</div>
 				<div className={styles.sub_menu_right}>
 					<div className={styles.search}>
-						<button className={styles.button}>검색</button>
+						<button className={styles.button} onClick={isSearchClick}>
+							<i className="fas fa-search"></i>
+						</button>
 					</div>
 					<div>
 						<img className={styles.img} src={profileImg} alt="profile_img" />
--- src/components/views/navbar/projectListModal/ProjectListModal.tsx
+++ src/components/views/navbar/projectListModal/ProjectListModal.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import styles from './sections/ProjectListModal.module.css';
+import styles from './sections/projectListModal.module.css';
 
 const ProjectListModal = () => {
 	return (
-		<section className={styles.modal}>
-			<ul>
+		<section className={styles.container}>
+			<div className={styles.close_btn}>
+				<i className="fas fa-times"></i>
+			</div>
+			<ul className={styles.ul}>
 				<li>내 프로젝트</li>
 				<li>팀 프로젝트</li>
 			</ul>
-			<div>
-				<button>새 프로젝트 만들기</button>
+			<div className={styles.div}>
+				<button className={styles.project_add_btn}>
+					<i className="fas fa-folder-plus"></i>
+				</button>
+				<span>새 프로젝트 만들기</span>
 			</div>
 		</section>
 	);
