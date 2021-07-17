eec35df038c6f876884d45b6b05aa9dc489385a8
jiye-7 <jy522hi@gmail.com>
2021-07-17 23:19:32 +0900

    💄 Update Project Component CSS

--- src/components/views/projectSetting/ProjectSetting.jsx
+++ src/components/views/projectSetting/ProjectSetting.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import styles from './sections/projectSetting.module.css';
 import searchResult from '../modal/search/sections/searchResult';
 import ProjectMember from './ProjectMember';
+import Nav from '../nav/Nav';
 
 const ProjectSetting = (props) => {
 	return (
-		<div className={styles.container}>
-			<h2 className={styles.h2}>프로젝트 설정 창</h2>
-			<h3 className={styles.h3}>프로젝트 이름</h3>
-			<ul className={styles.ul}>
-				{searchResult.map((user) => {
-					return <ProjectMember key={user.id} user={user} />;
-				})}
-			</ul>
-		</div>
+		<>
+			<Nav />
+			<div className={styles.container}>
+				<h2 className={styles.h2}>프로젝트 설정 창</h2>
+				<h3 className={styles.h3}>프로젝트 이름</h3>
+				<ul className={styles.ul}>
+					{searchResult.map((user) => {
+						return <ProjectMember key={user.id} user={user} />;
+					})}
+				</ul>
+			</div>
+		</>
 	);
 };
 
